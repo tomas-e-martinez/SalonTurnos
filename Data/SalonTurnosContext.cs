a7d377c9e34eb632ea7d6ecc24636404ea3399ac
Tomás Martínez <tomas.emart@gmail.com>
2025-03-26 23:24:52 -0300

    configuracion efcore y migracion inicial

---
+++ Data/SalonTurnosContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalonTurnos.Models;
+
+namespace SalonTurnos.Data
+{
+    public class SalonTurnosContext : DbContext
+    {
+        public SalonTurnosContext(DbContextOptions<SalonTurnosContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Turno> Turnos { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SalonTurnos.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SalonTurnosContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalonTurnosConnection")));
 
 var app = builder.Build();
 
