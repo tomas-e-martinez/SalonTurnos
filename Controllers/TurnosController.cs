b295e8104e282640fb59ab8f9ed106a4557146ec
Tomás Martínez <tomas.emart@gmail.com>
2025-03-30 01:39:17 -0300

    added controller de turnos, turnoService y view turnos/index, api conectada

---
+++ Controllers/TurnosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalonTurnos.Services;
+
+namespace SalonTurnos.Controllers
+{
+    public class TurnosController : Controller
+    {
+        private readonly TurnoService _turnoService;
+
+        public TurnosController(TurnoService turnoService)
+        {
+            _turnoService =turnoService;
+        }
+
+        public async Task<IActionResult> Index(bool? disponible, int? salonId)
+        {
+            var turnos = await _turnoService.GetTurnosAsync(disponible, salonId);
+
+            return View(turnos);
+        }
+    }
+}
---
+++ Services/TurnoService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using SalonTurnos.Models;
+
+namespace SalonTurnos.Services
+{
+    public class TurnoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TurnoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("http://localhost:5016/api/");
+        }
+
+        public async Task<List<Turno>> GetTurnosAsync(bool? disponible, int? salonId)
+        {
+            var url = "turnos";
+
+            bool firstParameter = true;
+
+            if (disponible.HasValue)
+            {
+                url += $"?disponible={disponible.Value}";
+                firstParameter = false;
+            }
+
+            if (salonId.HasValue)
+            {
+                if (firstParameter)
+                {
+                    url += $"?salonId={salonId.Value}";
+                    firstParameter = false;
+                }
+                else
+                {
+                    url += $"&salonId={salonId.Value}";
+                }
+            }
+
+            var response = await _httpClient.GetStringAsync(url);
+            var turnos = JsonConvert.DeserializeObject<List<Turno>>(response);
+            return turnos;
+        }
+    }
+}
