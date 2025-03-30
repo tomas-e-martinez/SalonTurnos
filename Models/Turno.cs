833919312d4e6f8efb7f0e03247cddaf6f8b86c1
Tomás Martínez <tomas.emart@gmail.com>
2025-03-30 00:47:56 -0300

    datos cliente en modelo de turno pueden ser null, prop estado => bool disponible

--- Models/Turno.cs
+++ Models/Turno.cs
@@ -2,19 +2,12 @@
 {
     public class Turno
     {
-        public enum EstadoTurno
-        {
-            Disponible,
-            Reservado,
-            Cancelado
-        }
-
         public int Id { get; set; }
         public int SalonId { get; set; }
         public DateTime FechaHora { get; set; }
-        public EstadoTurno Estado { get; set; }
-        public string ClienteNombre { get; set; }
-        public string ClienteEmail { get; set; }
-        public string ClienteTelefono { get; set; }
+        public bool Disponible { get; set; }
+        public string? ClienteNombre { get; set; }
+        public string? ClienteEmail { get; set; }
+        public string? ClienteTelefono { get; set; }
     }
 }
